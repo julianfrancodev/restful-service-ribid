890c119b85024632fd52287ff1bd911d89fc1583
julian <juliandanielfranco@gmail.com>
2021-03-02 14:18:58 -0500

    remove unused imports

--- app/Events/PostPublished.php
+++ app/Events/PostPublished.php
@@ -5,7 +5,6 @@ namespace App\Events;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
